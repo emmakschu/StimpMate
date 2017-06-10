a6ff928ca2f0bf956fcb553e3526665c3d0f3424
Michael K Schumacher <mschu528@gmail.com>
2017-06-10 07:53:54 -0500

    add sql order to stimp models

--- src/app/models/stimp.rb
+++ src/app/models/stimp.rb
@@ -1,26 +1,26 @@
 class Stimp < ActiveRecord::Base
 
   def self.woodsNative()
-    where("course = 'woods'").where("soilType = 'native'").order(:created_at, :desc)
+    where("course = 'woods'").where("soilType = 'native'").order("created_at DESC")
   end
 
   def self.woodsSand()
-    where("course = 'woods'").where("soilType = 'sand'").order(:created_at, :desc)
+    where("course = 'woods'").where("soilType = 'sand'").order("created_at DESC")
   end
 
   def self.prairieNative()
-    where("course = 'prairie'").where("soilType = 'native'")
+    where("course = 'prairie'").where("soilType = 'native'").order("created_at DESC")
   end
 
   def self.prairieSand()
-    where("course = 'prairie'").where("soilType = 'sand'")
+    where("course = 'prairie'").where("soilType = 'sand'").order("created_at DESC")
   end
 
-  def lakesNative()
-    where("course = 'lakes'").where("soilType = 'native'")
+  def self.lakesNative()
+    where("course = 'lakes'").where("soilType = 'native'").order("created_at DESC")
   end
 
-  def lakesSand()
-    where("course = 'lakes'").where("soilType = 'sand'")
+  def self.lakesSand()
+    where("course = 'lakes'").where("soilType = 'sand'").order("created_at DESC")
   end
 end
